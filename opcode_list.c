b50e2202e938b1cabaf52465c10b3bedda103801
Michael Brover <lepovan1999@gmail.com>
2026-05-20 19:56:03 +0300

    added opcode logic to help the parser

--- opcode_list.c
+++ opcode_list.c
@@ -2,24 +2,76 @@
 #include "opcode_list.h"
 
 static CommandInfo commands[] = {
-    {"mov",  0, 2},
-    {"cmp",  1, 2},
-    {"add",  2, 2},
-    {"sub",  3, 2},
-    {"lea",  4, 2},
 
-    {"clr",  5, 1},
-    {"not",  6, 1},
-    {"inc",  7, 1},
-    {"dec",  8, 1},
-    {"jmp",  9, 1},
-    {"bne", 10, 1},
-    {"red", 11, 1},
-    {"prn", 12, 1},
-    {"jsr", 13, 1},
+    /* two operands */
 
-    {"rts", 14, 0},
-    {"stop", 15, 0}
+    {"mov", 0, -1, TWO_OPERANDS,
+        ADDR_IMMEDIATE | ADDR_DIRECT | ADDR_REGISTER,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"cmp", 1, -1, TWO_OPERANDS,
+        ADDR_IMMEDIATE | ADDR_DIRECT | ADDR_REGISTER,
+        ADDR_IMMEDIATE | ADDR_DIRECT | ADDR_REGISTER},
+
+    {"add", 2, 10, TWO_OPERANDS,
+        ADDR_IMMEDIATE | ADDR_DIRECT | ADDR_REGISTER,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"sub", 2, 11, TWO_OPERANDS,
+        ADDR_IMMEDIATE | ADDR_DIRECT | ADDR_REGISTER,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"lea", 4, -1, TWO_OPERANDS,
+        ADDR_DIRECT,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    /* one operand */
+
+    {"clr", 5, 10, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"not", 5, 11, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"inc", 5, 12, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"dec", 5, 13, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"jmp", 9, 10, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_RELATIVE},
+
+    {"bne", 9, 11, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_RELATIVE},
+
+    {"jsr", 9, 12, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_RELATIVE},
+
+    {"red", 12, -1, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_DIRECT | ADDR_REGISTER},
+
+    {"prn", 13, -1, ONE_OPERAND,
+        ADDR_NONE,
+        ADDR_IMMEDIATE | ADDR_DIRECT | ADDR_REGISTER},
+
+    /* no operands */
+
+    {"rts", 14, -1, NO_OPERANDS,
+        ADDR_NONE,
+        ADDR_NONE},
+
+    {"stop", 15, -1, NO_OPERANDS,
+        ADDR_NONE,
+        ADDR_NONE}
 };
 
 #define COMMAND_COUNT (sizeof(commands) / sizeof(commands[0]))
@@ -33,5 +85,10 @@ CommandInfo *find_command(const char *name) {
         }
     }
 
+
+
     return NULL;
+}
+int is_addressing_legal(int legal_modes, int mode) {
+    return (legal_modes & mode) != 0;
 }
--- opcode_list.h
+++ opcode_list.h
@@ -1,12 +1,29 @@
 #ifndef OPCODE_TABLE_H
 #define OPCODE_TABLE_H
+#define ADDR_IMMEDIATE 1
+#define ADDR_DIRECT    2
+#define ADDR_RELATIVE  4
+#define ADDR_REGISTER  8
+#define ADDR_NONE      0
+
+
+typedef enum {
+    TWO_OPERANDS,
+    ONE_OPERAND,
+    NO_OPERANDS
+} OperandNum;
+
 
 typedef struct {
     char *name;
     int opcode;
-    int operand_count;
+    int funct;
+    OperandNum opr_num;
+    int legal_src;
+    int legal_dst;
 } CommandInfo;
 
 CommandInfo *find_command(const char *name);
+int is_addressing_legal(int legal_modes, int mode);
 
 #endif
