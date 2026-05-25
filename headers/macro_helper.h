0f106cc7790f50057c04af10d26540b01a8b7e21
Michael Brover <lepovan1999@gmail.com>
2026-05-25 15:59:04 +0300

    added comments

--- headers/macro_helper.h
+++ headers/macro_helper.h
@@ -23,6 +23,6 @@ Macro *find_macro(MacroList *list, const char *name);
 void add_line_to_macro(Macro *m, const char *line);
 void free_macro_list(MacroList *list);
 
-void expand_macros(FILE *src, FILE *out, MacroList *macros);
+int expand_macros(FILE *src, FILE *out, MacroList *macros);
 
 #endif
---
+++ headers/util.h
@@ -0,0 +1,5 @@
+#ifndef UTIL_H
+#define UTIL_H
+char *str_dup(const char *s);
+void trim(char *str);
+#endif
